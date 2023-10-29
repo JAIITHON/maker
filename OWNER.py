37aae4b465295d712054546866c451b7ab9d930d
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-10-29 02:46:57 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["Zo_Mbi_e"]
-OWNER_NAME = "zombie"
+OWNER = ["BENN_DEV"]
+OWNER_NAME = "ben"
 BOT_TOKEN = "6553005927:AAH2CTrIlBh4x9xw_9InmhX9EkD0W38ti64"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
-CHANNEL = "https://t.me/zombieyy"
-GROUP = "https://t.me/zombie2"
+CHANNEL = "https://t.me/BENfiles"
+GROUP = "https://t.me/BENsupport"
 VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
 LOGS = "zombie2"
 
