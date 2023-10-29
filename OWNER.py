b8ba80226e2ba7596815cf1a3da73bb9297e17e0
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-10-29 04:05:37 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -4,7 +4,7 @@ OWNER_NAME = "ᯓ 𓆩 ˹𝙱𝙴𝙽˼ 𓆪 #1"
 BOT_TOKEN = "6553005927:AAH2CTrIlBh4x9xw_9InmhX9EkD0W38ti64"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/BENfiles"
-GROUP = "https://t.me/BENsupport"
+GROUP = "https://t.me/BENNsupport"
 VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
 LOGS = "zombie2"
 
