cc3cd6cc277d4a10608571e3a46edddd543274ff
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-10-29 04:05:11 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,6 +1,6 @@
 # متطلبات التنصيب #
 OWNER = ["BENN_DEV"]
-OWNER_NAME = "ben"
+OWNER_NAME = "ᯓ 𓆩 ˹𝙱𝙴𝙽˼ 𓆪 #1"
 BOT_TOKEN = "6553005927:AAH2CTrIlBh4x9xw_9InmhX9EkD0W38ti64"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/BENfiles"
